c0fc4a6bbf5d3f2f3508be2d39a9e51e707127fa
Imed Boumalek <imedboumalek@gmail.com>
2022-12-26 15:34:16 +0100

    partial revert

--- blog/pages/projects.tsx
+++ blog/pages/projects.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Projects I'm most proud of
+            Showcase your projects with a hero image (16 x 9)
           </p>
         </div>
         <div className="container py-12">
